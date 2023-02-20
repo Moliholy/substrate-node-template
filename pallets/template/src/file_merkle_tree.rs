e441610efdbafa51f1a40dcad0e0cbd499e71ce1
José Molina <jose.molina@criptan.com>
2023-02-20 02:00:18 +0100

    Add number of pieces

--- pallets/template/src/file_merkle_tree.rs
+++ pallets/template/src/file_merkle_tree.rs
@@ -1,20 +1,79 @@
+use codec::{Decode, Encode, EncodeLike};
+use scale_info::{Path, Type, TypeInfo};
+use scale_info::build::Fields;
 use sp_io::hashing::sha2_256;
-use sp_std::vec::{Vec};
+use sp_std::vec::Vec;
 use sp_std::vec;
 
 // 1KB chunk size
 const CHUNK_SIZE: usize = 1024;
 const CHUNK_FILLER: [u8; 32] = [0u8; 32];
 
+
+#[derive(Default, Clone, PartialEq)]
 pub struct FileMerkleTree {
     pub file_bytes: Vec<u8>,
     pub merkle_tree: Vec<u8>,
+    pub pieces: u32,
 }
+
+impl Encode for FileMerkleTree {
+    fn encode(&self) -> Vec<u8> {
+        let file_size = (self.file_bytes.len() as u32).to_le_bytes();
+        let mut result = Vec::from(file_size.as_slice());
+        result.extend_from_slice(self.pieces.to_le_bytes().as_slice());
+        result.extend_from_slice(&self.file_bytes.encode());
+        result.extend_from_slice(&self.merkle_tree.encode());
+        result
+    }
+}
+
+impl Decode for FileMerkleTree {
+    fn decode<I: codec::Input>(input: &mut I) -> Result<Self, codec::Error> {
+        let mut buff = [0u8; 4];
+        input.read(&mut buff)?;
+        let file_size = u32::from_le_bytes(buff);
+        input.read(&mut buff)?;
+        let pieces = u32::from_le_bytes(buff);
+        let mut file_bytes = vec![0u8; file_size as usize];
+        input.read(&mut file_bytes)?;
+        let mut merkle_tree = vec![0u8; file_size as usize];
+        input.read(&mut merkle_tree)?;
+        Ok(FileMerkleTree { file_bytes, merkle_tree, pieces })
+    }
+}
+
+impl TypeInfo for FileMerkleTree {
+    type Identity = Self;
+
+    fn type_info() -> Type {
+        Type::builder().path(Path::new("FileMerkleTree", module_path!())).composite(
+            Fields::named()
+                .field(|f| {
+                    f.ty::<Vec<u8>>()
+                        .name("file_bytes")
+                        .type_name("Vec<u8>")
+                })
+                .field(|f| {
+                    f.ty::<Vec<u8>>()
+                        .name("merkle_tree")
+                        .type_name("Vec<u8>")
+                })
+                .field(|f| {
+                    f.ty::<u32>()
+                        .name("pieces")
+                        .type_name("u32")
+                })
+        )
+    }
+}
+
+impl EncodeLike for FileMerkleTree {}
 
 impl FileMerkleTree {
     pub fn new(file_bytes: Vec<u8>) -> Self {
         let chunks = file_bytes.chunks(CHUNK_SIZE);
-        let size = chunks.len();
+        let pieces = chunks.len();
         let mut tree = chunks.map(|chunk| {
             if chunk.len() != CHUNK_SIZE {
                 // process last chunk
@@ -32,8 +91,8 @@ impl FileMerkleTree {
                 acc
             });
         // make the tree a totally balanced binary tree
-        let mut num_items = size.next_power_of_two();
-        for _ in 0..(num_items - size) {
+        let mut num_items = pieces.next_power_of_two();
+        for _ in 0..(num_items - pieces) {
             tree.extend_from_slice(&CHUNK_FILLER);
         }
         let mut pos = 0;
@@ -52,15 +111,13 @@ impl FileMerkleTree {
         }
         Self {
             file_bytes,
+            pieces: pieces as u32,
             merkle_tree: tree,
         }
     }
 
     pub fn file_chunk_at(&self, position: usize) -> &[u8] {
         let count = self.piece_count();
-        if position >= count {
-            panic!("Invalid position: {}", position);
-        }
         let pos = position * CHUNK_SIZE;
         let limit = if position == count - 1 {
             self.file_bytes.len()
@@ -73,7 +130,7 @@ impl FileMerkleTree {
         if len % CHUNK_SIZE == 0 { len / CHUNK_SIZE } else { (len / CHUNK_SIZE) + 1 }
     }
 
-    pub fn root_hash(&self) -> &[u8] {
+    pub fn merkle_root(&self) -> &[u8] {
         &self.merkle_tree[(self.merkle_tree.len() - CHUNK_SIZE)..self.merkle_tree.len()]
     }
 }
--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -9,16 +9,15 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+    use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use sp_io::hashing::blake2_256;
     use sp_std::vec::Vec;
     use crate::file_merkle_tree::FileMerkleTree;
 
-	#[pallet::pallet]
+    #[pallet::pallet]
     #[pallet::generate_store(pub (super) trait Store)]
-	#[pallet::without_storage_info]
-	pub struct Pallet<T>(_);
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(_);
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
@@ -26,59 +25,47 @@ pub mod pallet {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
 
-    // Pallets use events to inform users when important changes are made.
-    // Event documentation should end with an array that provides descriptive names for parameters.
+
     #[pallet::event]
     #[pallet::generate_deposit(pub (super) fn deposit_event)]
     pub enum Event<T: Config> {
         /// Event emitted when a claim has been created.
-        ClaimCreated { who: T::AccountId, claim: T::Hash },
-        /// Event emitted when a claim is revoked by the owner.
-        ClaimRevoked { who: T::AccountId, claim: T::Hash },
+        FileUploaded { who: T::AccountId, merkle_root: T::Hash, pieces: u32 },
     }
 
     #[pallet::error]
     pub enum Error<T> {
-        /// The claim already exists.
-        AlreadyClaimed,
-        /// The claim does not exist, so it cannot be revoked.
-        NoSuchClaim,
-        /// The claim is owned by another account, so caller can't revoke it.
-        NotClaimOwner,
+        /// Could not obtain the merkle root hash
+        Unhasheable,
     }
 
     #[pallet::storage]
-    pub(super) type Claims<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, (T::AccountId, T::BlockNumber)>;
+    #[pallet::getter(fn files)]
+    pub(super) type Files<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, (T::AccountId, FileMerkleTree)>;
 
-    // Dispatchable functions allow users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::weight(0)]
         #[pallet::call_index(0)]
-        pub fn create_claim(origin: OriginFor<T>, file_bytes: Vec<u8>) -> DispatchResult {
+        pub fn upload_file(origin: OriginFor<T>, file_bytes: Vec<u8>) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             // This function will return an error if the extrinsic is not signed.
-            let sender = ensure_signed(origin)?;
+            let who = ensure_signed(origin)?;
 
             let file_merkle_tree = FileMerkleTree::new(file_bytes.clone());
-            let hash = file_merkle_tree.root_hash().to_vec();
-            let claim = T::Hash::decode(
-                &mut hash.as_slice()
-            ).unwrap();
-
-            // Verify that the specified claim has not already been stored.
-            ensure!(!Claims::<T>::contains_key(&claim), Error::<T>::AlreadyClaimed);
-
-            // Get the block number from the FRAME System pallet.
-            let current_block = <frame_system::Pallet<T>>::block_number();
+            let merkle_root = T::Hash::decode(
+                &mut file_merkle_tree.merkle_root()
+            ).or(Err(Error::<T>::Unhasheable))?;
 
             // Store the claim with the sender and block number.
-            Claims::<T>::insert(&claim, (&sender, current_block));
+            Files::<T>::insert(&merkle_root, (&who, &file_merkle_tree));
 
             // Emit an event that the claim was created.
-            Self::deposit_event(Event::ClaimCreated { who: sender, claim });
+            Self::deposit_event(Event::FileUploaded {
+                who,
+                merkle_root,
+                pieces: file_merkle_tree.pieces
+            });
 
             Ok(())
         }
